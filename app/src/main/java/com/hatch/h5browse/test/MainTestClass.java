0b88c68e05df1c98662ed659102a14af6d7ff60b
hcy <chuanyihuang@pptv.com>
2018-08-09 14:47:42 +0800

    test

--- app/src/main/java/com/hatch/h5browse/test/MainTestClass.java
+++ app/src/main/java/com/hatch/h5browse/test/MainTestClass.java
@@ -1,19 +1,121 @@
 package com.hatch.h5browse.test;
 
-import android.text.TextUtils;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 
 public class MainTestClass {
     public static void main(String[] arg) {
-        readLineFile("D:\\Tencent data\\503683716\\FileRecv\\MobileFile\\AdguardFilters-master\\MobileFilter\\sections\\adservers.txt");
+        String url = "http://bbs.tianya.cn/m/post-house-252774-";//"http://bbs.tianya.cn/m/post-house-252774-50.shtml";
+        getData(url);
     }
+
+
+    private static void getData(final String url) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                ArrayList<String> list = new ArrayList<>();
+                //建立头文件
+                list.add("<html>");
+                list.add("<body style=\"margin: 0 auto; width: 800px; text-align: center;\">");
+
+
+                for (int i = 1; i <= 521; i++) {
+                    ArrayList<String> list2 = get(url + i + ".shtml");
+                    if (list.size() > 0) {
+                        list.addAll(list2);
+                    }
+                    try {
+                        int time = (int) (Math.random() * 6);
+                        if (time < 3) {
+                            time = 3;
+                        }
+                        System.out.println("第" + i + "页，等待时间为：" + time + ",数据条数：" + list2.size());
+                        Thread.sleep(1000 * time);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+
+                //建立结尾的文件
+                list.add("</body></html>");
+
+                String[] content = new String[list.size()];
+                System.out.println("===========结束，开始写数据，数据总体条数" + content.length);
+                list.toArray(content);
+                writeLineFile("D:\\getData.html", content);
+
+            }
+        }).start();
+    }
+
+
+    private static ArrayList<String> get(String urlStr) {
+        ArrayList<String> list = new ArrayList<>();
+        try {
+            //创建URL对象
+            URL url = new URL(urlStr);
+            //url.openConnection()是调用url对象上的openConnection方法，将得到一个URLConnection对象赋值给connection
+            URLConnection connection = url.openConnection();
+            //获取当前网络输入流
+            InputStream is = connection.getInputStream();
+            //字节到字符的转换
+            InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+            BufferedReader br = new BufferedReader(isr);
+            String line;
+            boolean isMain = false;
+            boolean isStart = false;
+            while ((line = br.readLine()) != null) {
+                if (line.contains("data-id=\"10391542\"")) {
+                    isMain = true;
+                }
+                if (isMain) {
+                    if (line.contains("<div class=\"reply-div\">")) {
+                        isStart = true;
+                    } else if (line.contains("class=\"time fc-gray\"")) {
+                        list.add(line);
+                    }
+                    if (isStart) {
+                        if (line.length() > 0) {
+                            list.add(line);
+                        }
+                        if (line.contains("</div>")) {
+                            list.add("<p>--------------------------------------------------------------------------------------------------------------------</p>");
+                            list.add("<p>--------------------------------------------------------分割线-------------------------------------------------------</p>");
+                            list.add("<p>--------------------------------------------------------------------------------------------------------------------</p>");
+                            isMain = false;
+                            isStart = false;
+                        }
+                    }
+                }
+//                System.out.print(line + "\n\r");
+            }
+            br.close();
+            isr.close();
+            is.close();
+        } catch (MalformedURLException e) {
+            System.out.println("-------MalformedURLException:" + e);
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("-------IOException:" + e);
+            e.printStackTrace();
+        }
+
+        return list;
+    }
+
 
     public static void readLineFile(String filename) {
         try {
